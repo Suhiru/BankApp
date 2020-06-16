52a122a67f6363977693c849d7400a49ec33494f
Suhiru Benjamin De Silva <66971839+Suhiru@users.noreply.github.com>
2020-06-16 23:16:05 +0530

    Add files via upload

---
+++ BankApplication.java
@@ -0,0 +1,11 @@
+public class BankApplication {
+
+    //Main method
+    public static void main(String []args){
+
+
+        BankAccount obj=new BankAccount("Suhiru Benjamin",4356);
+        obj.showMenu();
+
+    }
+}
